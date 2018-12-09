416b69e0a6341e0d610bab5244693639d82c8890
Humayun Khan <humayun.khan@mail.mcgill.ca>
2018-12-09 19:57:49 +0500

    solver faulty

---
+++ generator.js
@@ -0,0 +1,61 @@
+function build() {
+    let i, j;
+    let placeholder = document.getElementById("table-placeholder");
+    let text = (document.getElementById("expression")).value;
+    if (text == "") {
+        placeholder.innerHTML = "<div></div>";
+        return;
+    }
+    if (text.match(/[^ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01+'() ]/g) != null) {
+        placeholder.innerHTML = "<p>One of the characters is not allowed.</p>";
+        return;
+    }
+    text = text.replace(/ /g, '');
+    text = text.toUpperCase();
+    while (numOf(text, '(') > numOf(text, ')'))
+        text += ")";
+    let variables = [];
+    for (i = 0; i < text.length; i++) {
+        if ((text[i] >= 'A' && text[i] <= 'Z')) {
+            if (text.indexOf(text[i]) == i) {
+                variables.push(text[i]);
+            }
+        }
+    }
+    variables.sort();
+    if (variables.length > 8) {
+        placeholder.innerHTML = "<p>You can only have 8 variables at a time.</p>";
+        return;
+    }
+    let string = "<tr><th style=\"letter-spacing: 0; padding: initial;\">minterm</th>";
+    for (i = 0; i < variables.length; i++) {
+        string += "<th>" + variables[i] + "</th>";
+    }
+    string += "<th>" + text + "</th></tr>";
+    for (i = 0; i < Math.pow(2, variables.length); i++) {
+        string += "<tr><td style=\"letter-spacing: 0; padding: initial;\">"+i.toString()+"</td>";
+        let data = [];
+        for (j = 0; j < variables.length; j++) {
+            data[j] = Math.floor(i / Math.pow(2, variables.length - j - 1)) % 2;
+            string += "<td>" + data[j] + "</td>";
+        }
+        let equation = text;
+        for (j = 0; j < variables.length; j++) {
+            equation = equation.replace(new RegExp(variables[j], 'g'), data[j]);
+        }
+        string += "<td>" + solve(equation) + "</td></tr>";
+    }
+    string = "<table align='center' id>" + string + "</table>";
+    if (string.indexOf("<td></td>") == -1)
+        placeholder.innerHTML = string;
+    else
+        placeholder.innerHTML = "<p>Invalid expression.</p>";
+
+    function numOf(text, search) {
+        let count = 0;
+        for (let i = 0; i < text.length; i++)
+            if (text[i] == search)
+                count++;
+        return count;
+    }
+}
